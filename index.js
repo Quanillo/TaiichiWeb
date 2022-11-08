8eecd71f3ac513b47519edc278f689ae693e3dfe
Juan Garrigos <j.garrigos91@gmail.com>
2022-11-08 12:32:21 +0100

    Working...

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 let msg = document.getElementById('msg');
-let form = document.getElementById('form');
+let formSiono = document.getElementById('formSiono');
+let formManodedios = document.getElementById('formManodedios');
+let formIlumina = document.getElementById('formIlumina');
 let text = '';
-function submitHandler(e) {
-    let dato = document.getElementById('preguntaSiono')[0];
-    console.log(dato);
+
+//SIONO
+function submitSiono(e) {
+    let question = document.getElementById('preguntaSiono').value;
+    sionoResponse(question);
+    window.location = "#hero";
     e.preventDefault();
   }
-  form.addEventListener("submit", submitHandler);
+formSiono.addEventListener("submit", submitSiono);
 
-msg.innerHTML = text;
+function sionoResponse (question){
+  let n = Math.round(Math.random() * (2 - 1) + 1);
+  if(n%2==0){
+    msg.innerHTML = question + '? <br> SI.';
+  }
+  else{
+    msg.innerHTML = question + '? <br> NO.';
+  }
+}
+
+//MANODEDIOS
+function submitManodedios(e) {
+  let question = document.getElementById('preguntaManodedios').value;
+  manodediosResponse(question);
+  e.preventDefault();
+}
+formManodedios.addEventListener("submit", submitManodedios);
+
+const manodediosOptions = [];
+
+function manodediosResponse (question){
+  manodediosOptions.push(question);
+  console.log(manodediosOptions);
+}
+
+function manodediosChoice (){
+  let n = Math.round(Math.random() * (manodediosOptions.length - 0) + 0);
+  msg.innerHTML = 'Elección suprema: ' + '<br>' + manodediosOptions[n];
+  window.location = "#hero";
+}
+
+//ILUMINA
+function submitIlumina(e) {
+  let question = document.getElementById('preguntaIlumina').value;
+  iluminaResponse(question);
+  window.location = "#hero";
+  e.preventDefault();
+}
+formIlumina.addEventListener("submit", submitIlumina);
+
+function iluminaResponse (question){
+  let n = Math.round(Math.random() * (iluminaOptions.length - 0) + 0);
+  msg.innerHTML = question + '<br>' + iluminaOptions[n];
+}
+
+const iluminaOptions = [
+  'Cierra los ojos, y deja que Taiichi guíe tus dedos.',
+  'No renuncies a tus sueños, sigue durmiendo.',
+  'Estoy ocupado peinando a mi tortuga.',
+  'Todos somos contingentes, pero tú eres necesario.',
+]
+
+
+
