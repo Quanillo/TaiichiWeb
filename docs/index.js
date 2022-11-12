6acab39949c45385bc57a45966db0df934f9fec7
Juan <j.garrigos91@gmail.com>
2022-11-12 13:23:03 +0100

    Beta revised

--- docs/index.js
+++ docs/index.js
@@ -1,73 +1,82 @@
-let msg = document.getElementById('msg');
-let title = document.getElementById('title');
-let formSiono = document.getElementById('formSiono');
-let formManodedios = document.getElementById('formManodedios');
-let formIlumina = document.getElementById('formIlumina');
+//================  SIONO  ==========================
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('formSiono').addEventListener('submit', submitSiono); 
+});
 
-//SIONO
 function submitSiono(e) {
     e.preventDefault();
-    let question = document.getElementById('preguntaSiono').value;
+    let question = document.getElementById("preguntaSiono").value;
     e.target.reset();
     sionoResponse(question);
   }
-formSiono.addEventListener("submit", submitSiono);
 
 function sionoResponse (question){
   let n = Math.round(Math.random() * (2 - 1) + 1);
-  title.remove();
+  if(document.getElementById('title') != null)
+    document.getElementById('title').remove();
+   
   if(question.length>400){
-    msg.innerHTML = 'Mucho texto. Sintentiza.';
+    document.getElementById('msg').innerHTML.innerHTML = 'Mucho texto. Sintentiza.';
   }
   else if(n%2==0){
-    msg.innerHTML = question + '<br> SI.';
+    document.getElementById('msg').innerHTML = question + '<br> SI.';
     window.location = "#hero";
   }
   else{
-    msg.innerHTML = question + '<br> NO.';
+    document.getElementById('msg').innerHTML = question + '<br> NO.';
     window.location = "#hero";
   }
 }
 
-//MANODEDIOS
+//================  MANODEDIOS  ==========================
+let manodediosOptions = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('formManodedios').addEventListener('submit', submitManodedios); 
+});
+
 function submitManodedios(e) {
   e.preventDefault();
   let question = document.getElementById('preguntaManodedios').value;
   e.target.reset();
   manodediosResponse(question);
 }
-formManodedios.addEventListener("submit", submitManodedios);
-
-let manodediosOptions = [];
 
 function manodediosResponse (question){
-  title.remove();
-  manodediosOptions.push(question);
+  if(question !== null && question!=='')
+    manodediosOptions.push(question);
 }
 
 function manodediosChoice (){
-  console.log(manodediosOptions);
-  msg.innerHTML = 'Taiichi decide: ' + '<br>' + manodediosOptions[Math.floor(Math.random() * manodediosOptions.length)];
+  if(document.getElementById('title') != null)
+    document.getElementById('title').remove();
+
+  document.getElementById('msg').innerHTML = 'Taiichi decide: ' + '<br>' + manodediosOptions[Math.floor(Math.random() * manodediosOptions.length)];
   manodediosOptions = [];
   window.location = "#hero";
 }
 
-//ILUMINA
+//================  ILUMINA  ==========================
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('formIlumina').addEventListener('submit', submitIlumina); 
+});
+
 function submitIlumina(e) {
   e.preventDefault();
   let question = document.getElementById('preguntaIlumina').value;
   e.target.reset();
   iluminaResponse(question);
 }
-formIlumina.addEventListener("submit", submitIlumina);
 
 function iluminaResponse (question){
-  title.remove();
+  if(document.getElementById('title') != null)
+    document.getElementById('title').remove();
+
   if(question.length>400){
-    msg.innerHTML = 'Mucho texto. Sintentiza.';
+    document.getElementById('msg').innerHTML = 'Mucho texto. Sintentiza.';
   }
   else{
-    msg.innerHTML = question + '<br>' + iluminaOptions[Math.floor(Math.random() * iluminaOptions.length)];
+    document.getElementById('msg').innerHTML = question + '<br>' + iluminaOptions[Math.floor(Math.random() * iluminaOptions.length)];
     window.location = "#hero";
   }
 }
