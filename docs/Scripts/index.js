41851354f3f676c9796f67aa349f346ba30fca2a
Juan <j.garrigos91@gmail.com>
2022-11-13 16:55:39 +0100

    Añadiendo frases en Ilumina

--- docs/Scripts/index.js
+++ docs/Scripts/index.js
@@ -3,7 +3,6 @@ function goToPageDelay(page, n){
     document.location = "#" + page;
   }, n);
 }
-
 //================  SIONO  ==========================
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('formSiono').addEventListener('submit', submitSiono); 
@@ -20,7 +19,7 @@ function sionoResponse (question){
   let n = Math.round(Math.random() * (2 - 1) + 1);
   if(document.getElementById('title') != null)
     document.getElementById('title').remove();
-   
+
   if(question.length>400){
     document.getElementById('msg').innerHTML.innerHTML = 'Mucho texto. Sintentiza.';
   }
@@ -94,5 +93,18 @@ const iluminaOptions = [
   'No renuncies a tus sueños, sigue durmiendo.',
   'Estoy ocupado peinando a mi tortuga.',
   'Todos somos contingentes, pero tú eres necesario.',
+  'Todos deberíamos creer en algo... yo, por ejemplo, creo que tomaré otra copa.',
+  'La gente no sabe lo que tiene hasta que recoge su cuarto.',
+  '10 millones de moscas no se equivocan, ¡¡coma MIERDA!!',
+  'La ignorancia es temporal, la estupidez es para siempre.',
+  'Me gustan los largos paseos, especialmente cuando los toma gente molesta.',
+  'La edad es algo que no importa, a menos que sea usted un queso.',
+  'Un día sin sol es, ya sabes, de noche...',
+  'Si no puedes convencerlos, confúndelos.',
+  'Hay que fabricar máquinas que nos permitan seguir fabricando máquinas, porque lo que no va a hacer nunca la máquina es fabricar máquinas.',
+  'Un vaso es un vaso y un plato es un plato.',
+  'Lo más importante que se puede hacer por vosotros es lo que vosotros podáis hacer por vosotros.',
+  'No he dormido nada, no me pregunten demasiado',
+  'A veces lo mejor es no tomar decisiones, y eso en sí, es una decisión',
 ]
 //============= beta================
